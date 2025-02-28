04cb328855f2c003593a65b57c2cc07180221b33
Anton Kostiuk <ak4932328@gmail.com>
2025-02-28 16:57:32 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AppHeader/>
+  <div>
+    <p>Count: {{ counter.count }}</p>
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+import AppHeader from '@/components/layouts/AppHeader.vue';
+
+const counter = useCounterStore()
+</script>
+
+
